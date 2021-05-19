d5aabb51ef5894e0cf80f08421ef1191c72c0ab8
bandgeekdante <drbenciv@ucalgary.ca>
2021-05-18 19:46:36 -0600

    make global phase prettier

--- src/Circuit.js
+++ src/Circuit.js
@@ -20,7 +20,7 @@ function renderSquare(i, placedGates, globalPhase) {
   )
 }
 
-function renderGate(y, x, placedGates, globalPhase) {
+function renderGate(y, x, placedGates) {
   if (y === 0) {
     if (x === 0) {
       return <SingleQubitGate name={'X'} y={y} x={x}/>
@@ -28,20 +28,37 @@ function renderGate(y, x, placedGates, globalPhase) {
       return <SingleQubitGate name={'Z'} y={y} x={x}/>
     } else if (x === 2) {
       return <SingleQubitGate name={'Y'} y={y} x={x}/>
-    } else if (x === 7) {
-      return <div>Global phase = {globalPhase}</div>
     }
   } else if (placedGates[y][x]) {
     return <SingleQubitGate name={placedGates[y][x]} y={y} x={x}/>
   }
 }
 
+function renderGlobalPhase(globalPhase) {
+  return (
+    <div
+      key={56}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '12.5%',
+        width: '100%',
+        fontSize: 25,
+        fontWeight: 'bold',
+      }}
+    >
+      Global phase = {globalPhase}
+    </div>
+  )
+}
+
 export default function Circuit({ placedGates, globalPhase }) {
   const squares = []
-  for (let i = 0; i < 64; i++) {
-    squares.push(renderSquare(i, placedGates, globalPhase))
+  for (let i = 0; i < 56; i++) {
+    squares.push(renderSquare(i, placedGates))
   }
-
+  squares.push(renderGlobalPhase(globalPhase))
   return (
     <DndProvider
       options={HTML5toTouch}
